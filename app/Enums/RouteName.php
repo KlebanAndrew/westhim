ae5c62c2e47834cfd7c70312a77d52b9d0b7b2dd
klebanandrew <a.kleban@tenantcloud.com>
2024-12-16 17:36:57 +0200

    Added single service page.

---
+++ app/Enums/RouteName.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+final class RouteName
+{
+	public const HOME = 'home';
+	public const SERVICES = 'services';
+	public const SERVICES_SINGLE = 'services_single';
+	public const CONTACT = 'contact';
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\RouteName;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,5 +8,17 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-	return view('home');
-});
+	return view('pages.home');
+})->name(RouteName::HOME);
+
+Route::get('/services', function () {
+	return view('pages.services');
+})->name(RouteName::SERVICES);
+
+Route::get('/services-single', function () {
+	return view('pages.services-single');
+})->name(RouteName::SERVICES_SINGLE);
+
+Route::get('/contact', function () {
+	return view('pages.contact');
+})->name(RouteName::CONTACT);
